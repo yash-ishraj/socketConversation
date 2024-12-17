ca18c65bd5cf8e566255b4eca852f7804ef83495
yash-ishraj <as7488896@gmail.com>
2024-12-17 19:16:09 +0530

    Setted env variable

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const WebSocket = require('ws');
 const http = require('http');
 
+
+const port = process.env.PORT || 3000;
 // Create HTTP server
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
